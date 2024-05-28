ae7742342d80011e8d816f23a83651c5d69b7ff3
DimaKarpuk <99805543+DimaKarpuk@users.noreply.github.com>
2024-05-28 11:34:09 +0300

    added attach

--- src/test/java/tests/TestsBase.java
+++ src/test/java/tests/TestsBase.java
@@ -1,6 +1,10 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestsBase {
@@ -9,7 +13,14 @@ public class TestsBase {
         Configuration.browserSize = "1928x1080";  // меняем размер окна браузера
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager"; // не дожидаемся полной загрузки страницы сайта
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterEach
+    void addAttach(){
+        Attach.screenshotAs("Last screenshot");
+
+
     }
 
 }
--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,8 +1,4 @@
 package tests;
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -11,23 +7,13 @@ import utils.RandomUtils;
 
 import static io.qameta.allure.Allure.step;
 
-
-public class TextBoxTests{
+public class TextBoxTests extends TestsBase{
     RegistrationPage registrationPage = new RegistrationPage();
     RandomUtils randomUtils = new RandomUtils();
-
-    @BeforeAll
-    static void beforeAll(){
-        Configuration.browserSize = "1928x1080";  // меняем размер окна браузера
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager"; // не дожидаемся полной загрузки страницы сайта
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-    }
     @Tag("simple")
     @DisplayName("Тест на регистрацию с полными данными")
     @Test
     void fillFormTest() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
             step("Открываем страницу регистрации", () -> {
                 registrationPage.openPage();
             });
@@ -63,7 +49,6 @@ public class TextBoxTests{
     @DisplayName("Тест на регистрацию с минимальными данными")
     @Test
     void partialRegistrationTest(){
-        SelenideLogger.addListener("allure", new AllureSelenide());
             step("Открываем страницу регистрации", () -> {
                 registrationPage.openPage();
         });
@@ -85,14 +70,13 @@ public class TextBoxTests{
             });
     }
     @Tag("simple")
-    @DisplayName("Тест на регистрацию с негативными данными")
+    @DisplayName("Негативный тест на регистрацию с неполными данными")
     @Test
     void invalidRegistrationTest(){
-        SelenideLogger.addListener("allure", new AllureSelenide());
             step("Открываем страницу регистрации", () -> {
                 registrationPage.openPage();
             });
-            step("Заполняем форму регистрации с негативными данными", () -> {
+            step("Заполняем форму регистрации с неполными данными", () -> {
                 registrationPage.setFirstName(randomUtils.randomFirstName)
                         .setLastName(randomUtils.randomLastName)
                         .setGender(randomUtils.randomGender)
