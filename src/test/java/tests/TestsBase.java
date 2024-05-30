b4c3fb51aa37d78936c5d56f9239845b7dda0444
DimaKarpuk <99805543+DimaKarpuk@users.noreply.github.com>
2024-05-30 21:24:35 +0300

    updated beforeAll

--- src/test/java/tests/TestsBase.java
+++ src/test/java/tests/TestsBase.java
@@ -10,22 +10,23 @@ import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
-
 public class TestsBase {
     @BeforeAll
     static void beforeAll(){
-        Configuration.browserSize = "1928x1080";  // меняем размер окна браузера
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager"; // не дожидаемся полной загрузки страницы сайта
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
+            Configuration.browserSize = System.getProperty("size","1928x1080");
+            Configuration.browser = System.getProperty("browser","chrome");
+            Configuration.browserVersion = System.getProperty("version","120.0");
+            Configuration.baseUrl = "https://demoqa.com";
+            Configuration.pageLoadStrategy = "eager"; // не дожидаемся полной загрузки страницы сайта
+            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+            DesiredCapabilities capabilities = new DesiredCapabilities();// добавляем видео запись
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+            Configuration.browserCapabilities = capabilities;
 
-        SelenideLogger.addListener("allure", new AllureSelenide());
+            SelenideLogger.addListener("allure", new AllureSelenide());
     }
     @AfterEach
     void addAttach(){
